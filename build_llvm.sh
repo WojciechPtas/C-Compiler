6278fa7e80d84533d40204a77c92402adb8cb52d
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2020-11-26 10:51:45 +0100

    Update script for building LLVM

--- build_llvm.sh
+++ build_llvm.sh
@@ -7,24 +7,19 @@ cd llvm
 CUR=`pwd`
 
 if [ ! -e  "${CUR}/llvm" ]; then
-        wget http://releases.llvm.org/7.0.0/llvm-7.0.0.src.tar.xz
-        tar xf llvm-7.0.0.src.tar.xz
-        rm llvm-7.0.0.src.tar.xz
-        mv llvm-7.0.0.src llvm
-
-        # uncomment this to also build clang
-        # cd llvm/tools
-        # wget http://releases.llvm.org/7.0.0/cfe-7.0.0.src.tar.xz
-        # tar xf cfe-7.0.0.src.tar.xz
-        # rm cfe-7.0.0.src.tar.xz
-        # mv cfe-7.0.0.src clang
+        wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-project-11.0.0.tar.xz
+        tar xf llvm-project-11.0.0.tar.xz
+        rm llvm-project-11.0.0.tar.xz
+        mv llvm-project-11.0.0 llvm
 
         cd ${CUR}
 fi
 
 mkdir -p build
+mkdir -p install
 cd build
-cmake ../llvm \
+cmake ../llvm/llvm \
+        -DLLVM_ENABLE_PROJECTS='clang' \
         -DCMAKE_BUILD_TYPE:STRING=Debug \
         -DCMAKE_INSTALL_PREFIX:PATH="${CUR}/install" \
         -DLLVM_ENABLE_ASSERTIONS:BOOL=ON \
@@ -33,11 +28,16 @@ cmake ../llvm \
         -DLLVM_INCLUDE_TESTS:BOOL=OFF \
         -DLLVM_TARGETS_TO_BUILD="X86"
 
+
 # In the above command,
-# "-DCMAKE_BUILD_TYPE:STRING=Debug" enables debug information for this build,
-#       this simplifies debugging and provides better backtraces, but increases
-#       memory consumption of the build process as well as disk space
-#       consumption of the result significantly.
+# "-DLLVM_ENABLE_PROJECTS='clang'" enables building clang, LLVM's C/C++
+#       frontend. Remove this line to save memory and disk space.
+# "-DCMAKE_BUILD_TYPE:STRING=Debug" enables debug information for this build.
+#       This is useful for debugging and for better backtraces, at the cost of
+#       a larger memory consumption of the build process as well as disk space
+#       consumption of the result.
+#       Replace this with "-DCMAKE_BUILD_TYPE:STRING=Release" to sacrifice
+#       usability for reduced memory requirements.
 # "-DLLVM_ENABLE_ASSERTIONS:BOOL=ON" enables assertions that ensure validity
 #       constraints in the LLVM code, you should never disable them for
 #       development!
@@ -56,6 +56,5 @@ cmake ../llvm \
 make install
 
 # You can replace the above command with the following one to use more threads
-# for building LLVM (this increases the memory consumption significantly).
+# for building LLVM (this might increase the memory consumption significantly).
 # make -j2 install
-
