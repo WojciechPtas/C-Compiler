29792af53b2f5fc6c6492738e20fd7a32984b913
Luis Felger <lufe00001@stud.uni-saarland.de>
2022-11-08 18:33:09 +0100

    Add example to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include "model/token/KeywordToken.h"
+#include "util/token/PrintVisitor.h"
+
 using namespace std;
 
-int main(int, char **const) {
-    cerr << "TODO: Implement a compiler." << std::endl;
-    return EXIT_FAILURE;
+int main(int argc, const char *argv[]) {
+    c4::model::token::KeywordToken kwToken(
+        c4::model::token::TokenPosition("test.c", 1, 1),
+        c4::model::token::Keyword::While
+    );
+
+    c4::util::token::PrintVisitor visitor(std::cout);
+
+    kwToken.accept(visitor);
+
+    return 0;
 }
