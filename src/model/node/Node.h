3ba2616e6197d2c8018bc6d8c0718b4e5fdbe891
Luis Felger <lufe00001@stud.uni-saarland.de>
2022-11-21 22:52:14 +0100

    Fix formatting

--- src/model/node/Node.h
+++ src/model/node/Node.h
@@ -9,17 +9,16 @@ namespace c4 {
         namespace node {
             template <class key, class Value>
             class Node{
-                    private:
-                        std::unordered_map<key, std::shared_ptr<Node>> transitions;
-                        std::shared_ptr<Value> result;
-                    public:
-                        Node();
-                        Node(std::vector<key>& word, const std::shared_ptr<Value> Token);
-                        bool addChildNode(key c, std::shared_ptr<Node> child);
-                        std::shared_ptr<Node<key, Value>> getChildNode(key c); // auto next=ournoude->getChildNode(c) if(next)
-                        void merge(const std::shared_ptr<Node> ptr);
-                    };
-
-            }
+                private:
+                    std::unordered_map<key, std::shared_ptr<Node>> transitions;
+                    std::shared_ptr<Value> result;
+                public:
+                    Node();
+                    Node(std::vector<key>& word, const std::shared_ptr<Value> Token);
+                    bool addChildNode(key c, std::shared_ptr<Node> child);
+                    std::shared_ptr<Node<key, Value>> getChildNode(key c); // auto next=ournoude->getChildNode(c) if(next)
+                    void merge(const std::shared_ptr<Node> ptr);
+            };
         }
+    }
 }
