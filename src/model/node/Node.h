d8e74e177911d0d8412d939fe85947b7d8f6347b
Luis Felger <lufe00001@stud.uni-saarland.de>
2022-11-22 11:01:59 +0100

    Fix generic implementation

--- src/model/node/Node.h
+++ src/model/node/Node.h
@@ -8,16 +8,43 @@ namespace c4 {
     namespace model {
         namespace node {
             template <class key, class Value>
-            class Node{
+            class Node {
                 private:
                     std::unordered_map<key, std::shared_ptr<Node>> transitions;
-                    std::shared_ptr<Value> result;
+                    std::shared_ptr<const Value> result;
+
                 public:
-                    Node();
-                    Node(std::vector<key>& word, const std::shared_ptr<Value> Token);
-                    bool addChildNode(key c, std::shared_ptr<Node> child);
-                    std::shared_ptr<Node<key, Value>> getChildNode(key c); // auto next=ournoude->getChildNode(c) if(next)
-                    void merge(const std::shared_ptr<Node> ptr);
+                    Node(std::shared_ptr<const Value> token) {
+                        this->result = token;
+                    }
+
+                    bool addChildNode(key c, std::shared_ptr<Node> child) {
+                        if(this->transitions.find(c)==this->transitions.end()){
+                            this->transitions[c]=child;
+                            return true;
+                        }
+                        
+                        return false;
+                    }
+
+                    std::shared_ptr<Node<key, Value>> getChildNode(key c) {
+                        if(this->transitions.find(c)==this->transitions.end()){
+                            return nullptr;
+                        }
+                        
+                        return this->transitions[c];
+                    }
+
+                    void merge(const std::shared_ptr<Node> ptr) {
+                        this->result=ptr->result;
+                        for(auto it = ptr->transitions.begin(); it!=ptr->transitions.end(); ++it){
+                            if(this->transitions.find(it->first)==this->transitions.end()){
+                                this->transitions[it->first]=it->second;
+                            }else{
+                                this->transitions[it->first]->merge(ptr->transitions[it->first]);
+                            }
+                        }
+                    }
             };
         }
     }
