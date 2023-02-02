071ce7047364b0b59ac8e2d7d98d4aacd9da9475
Wojciech.Ptas <wopt00001@stud.uni-saarland.de>
2023-02-02 19:17:20 +0100

    semantical analyser

---
+++ src/util/SemanticalAnalysis/SemanticalAnalyser.cpp
@@ -0,0 +1,231 @@
+#include "SemanticalAnalyser.h"
+#include "../../util/token/PrintVisitor.h"
+using namespace c4::util::sema;
+using namespace c4::model::declaration;
+using namespace c4::model::statement;
+using namespace c4::model::token;
+
+void c4::util::sema::SemanticalAnalyser::reportError(std::shared_ptr<model::token::Token> t, std::string msg)
+{
+    util::token::PrintVisitor v(os);
+    v.printPosition(*t);
+    os<<"error: " <<msg<<"\n";
+}
+// DONE!
+void c4::util::sema::SemanticalAnalyser::visit(const model::statement::CompoundStatement &s)
+{
+    declared_vars.push_back(std::set<std::string>());
+    variables_type.push_back(std::map<std::string,type>());
+    auto a= s;
+    for(auto& a : s.block_of_statements){
+        a->accept(*this);
+    }
+    declared_vars.pop_back();
+    variables_type.pop_back();
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::statement::ExpressionStatement &s)
+{
+    auto a= s;
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::statement::IterationStatement &s)
+{
+    //INSPECT EXPR
+    inLoop++;
+    if(s.statement!=nullptr){
+        s.statement->accept(*this);
+    }
+    inLoop--;
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::statement::JumpStatement &s)
+{
+    if(s.k==kind::_return){
+        // INSPECT EXPR
+    }
+    else if(s.k==kind::_goto){
+       if(labels.insert(std::make_pair(s.gotoIdentifier, false)).second){
+        gotoLabels[s.gotoIdentifier]=s.first_token;
+       }
+    }
+    else{
+        if(inLoop)
+        return;
+        else{
+            this->reportError(s.first_token,"continue and break are only allowed inside a loop");
+        }
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::statement::LabeledStatement &s)
+{
+    auto a = s;
+    labels[s.identifier]=true;
+    s.statement->accept(*this);
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::statement::SelectionStatement &s)
+{
+    // EXPR
+    auto a= s;
+    if(s.thenStatement!=nullptr) s.thenStatement->accept(*this);
+    if(s.elseStatement!=nullptr) s.elseStatement->accept(*this);
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::Declaration &s)
+{
+    auto a = s;
+    being_built.ptr_num=0;
+    s.ds->accept(*this);
+    if(s.declarator!=nullptr) s.declarator->accept(*this);
+    else{
+        reportError(s.first_token,"Declarations without declarators are not valid.");
+    }
+    if(isVar){
+        variables_type.back()[name]=being_built;
+        // os<< name << being_built.ptr_num;
+        // switch(being_built.simple){
+        //     case simple_type::_CHAR: os <<"char";
+        //     break;
+        //     case simple_type::_INT: os <<"int";
+        //     break;
+        //     case simple_type::_VOID: os <<"void";
+        //     break;
+        //     case simple_type::_STRUCT: os <<"struct";
+        //     break;
+        // }
+        // os<<"\n";
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::DeclarationSpecifier &s)
+{
+    auto a= s;
+    if(s.structorunion!=nullptr){
+        being_built.simple=simple_type::_STRUCT;
+        s.structorunion->accept(*this);
+    }
+    else{
+        switch(s.keyword){
+            case Keyword::Int: being_built.simple=simple_type::_INT;
+            break;
+            case Keyword::Char: being_built.simple=simple_type::_CHAR;
+            break;
+            case Keyword::Void: being_built.simple=simple_type::_VOID;
+            break;
+            default:
+            break;
+        }
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::Declarator &s)
+{
+    auto a= s;
+    if(s.ptr!=nullptr) s.ptr->accept(*this);
+    if(s.dec!=nullptr) s.dec->accept(*this);
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::DirectDeclarator &s)
+{
+    auto a= s;
+    
+    if(s.identifier!=""){
+        auto se = declared_vars.back();
+        if(se.find(s.identifier)!=se.end()){
+            std::string msg="Variable with this name: \"" + s.identifier + "\" was already declared in this scope";            
+            reportError(s.first_token,msg);
+            return;
+        }
+        else{
+            se.insert(s.identifier);
+            declared_vars.pop_back();
+            declared_vars.push_back(se);
+        }
+    }
+    ahead = true;
+    if(s.direct_declarator!=nullptr){
+        s.direct_declarator->accept(*this);
+    }
+    ahead=false;
+    if(s.identifier!=""){
+        name=s.identifier;
+    }
+    if(s.declarator!=nullptr){
+        s.declarator->accept(*this);
+    }
+
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::DirectDeclarator2 &s)
+{
+    auto a= s;
+    if(ahead){
+        isVar= !(s.declarator!=nullptr || s.list!=nullptr);
+        return;
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::FunctionDefinition &s)
+{
+    auto a= s;
+    labels.clear();
+    gotoLabels.clear();
+    // INSPECT BODY
+    //s.ds->accept(*this);
+    being_built.ptr_num=0;
+    //s.declarator->accept(*this);
+    // PUT INTO THE DICTIONARY
+    s.statement->accept(*this);
+    //CHECK IF ALL LABELS WERE PRESENT
+    for(auto it=labels.begin();it!=labels.end();it++){
+        if(!it->second){
+            std::string msg="label:\"" + it->first+ "\" not present";
+            reportError(gotoLabels[it->first],msg);
+        }
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::ParameterDeclaration &s)
+{
+    auto a= s;
+}
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::ParameterTypeList &s)
+{
+    auto a = s;
+    inParameter=true;
+    for(auto& a : s.params){
+        a->accept(*this);
+    }
+    inParameter=false;
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::Pointer &s)
+{
+    auto a= s;
+    being_built.ptr_num+=1;
+    if(s.ptr!=nullptr){
+        s.ptr->accept(*this);
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::Root &s)
+{
+    auto a= s;
+    for(auto& a : s.definitions){
+        a->accept(*this);
+    }
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::StructDeclarationList &s)
+{
+    auto a= s;
+
+}
+
+void c4::util::sema::SemanticalAnalyser::visit(const model::declaration::StructUnionSpecifier &s)
+{
+    auto a= s;
+
+}
---
+++ src/util/SemanticalAnalysis/SemanticalAnalyser.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <ostream>
+#include <map>
+#include <vector>
+#include <set>
+#include "../../model/IASTVisitor.h"
+#include "../../model/statement/CompoundStatement.h"
+#include "../../model/statement/ExpressionStatement.h"
+#include "../../model/statement/IterationStatement.h"
+#include "../../model/statement/JumpStatement.h"
+#include "../../model/statement/LabeledStatement.h"
+#include "../../model/statement/SelectionStatement.h"
+#include "../../model/declaration/Declaration.h"
+#include "../../model/declaration/DeclarationSpecifier.h"
+#include "../../model/declaration/Declarator.h"
+#include "../../model/declaration/DirectDeclarator.h"
+#include "../../model/declaration/DirectDeclarator2.h"
+#include "../../model/declaration/FunctionDefinition.h"
+#include "../../model/declaration/ParameterTypeList.h"
+#include "../../model/declaration/Pointer.h"
+#include "../../model/declaration/Root.h"
+#include "../../model/declaration/StructDeclarationList.h"
+#include "../../model/declaration/StructUnionSpecifier.h"
+#include "../../model/declaration/ParameterDeclaration.h"
+
+
+namespace c4::util::sema
+{
+    enum class simple_type{_VOID,_INT,_CHAR,_STRUCT};
+    struct type{
+        simple_type simple;
+        int ptr_num=0;
+    };
+    class SemanticalAnalyser : public model::statement::IASTVisitor{
+        std::ostream& os;
+        int inLoop;
+        bool inParameter;
+        std::map<std::string,bool> labels;
+        std::map<std::string, std::shared_ptr<model::token::Token>> gotoLabels;
+        std::vector<std::set<std::string>> declared_vars;
+        std::vector<std::map<std::string,type>> variables_type;
+        type being_built;
+        bool isVar;
+        std::string name;
+        bool ahead;
+        void reportError(std::shared_ptr<model::token::Token> t, std::string msg="wrong token");
+        public:
+        SemanticalAnalyser(std::ostream& os) : os(os), inLoop(0), inParameter(false), isVar(true), name(""),ahead(false)
+        {
+            declared_vars.push_back(std::set<std::string>());
+            variables_type.push_back(std::map<std::string,type>());
+        } //global range
+        void visit(const model::statement::CompoundStatement& s)override;
+        void visit(const model::statement::ExpressionStatement& s)override;
+        void visit(const model::statement::IterationStatement& s)override;
+        void visit(const model::statement::JumpStatement& s)override;
+        void visit(const model::statement::LabeledStatement& s)override;
+        void visit(const model::statement::SelectionStatement& s)override;
+        void visit(const model::declaration::Declaration& s)override;
+        void visit(const model::declaration::DeclarationSpecifier & s)override;
+        void visit(const model::declaration::Declarator& s)override;
+        void visit(const model::declaration::DirectDeclarator& s)override;
+        void visit(const model::declaration::DirectDeclarator2& s)override;
+        void visit(const model::declaration::FunctionDefinition& s)override;
+        void visit(const model::declaration::ParameterDeclaration& s)override;
+        void visit(const model::declaration::ParameterTypeList& s)override;
+        void visit(const model::declaration::Pointer& s)override;
+        void visit(const model::declaration::Root & s)override;
+        void visit(const model::declaration::StructDeclarationList & s)override;
+        void visit(const model::declaration::StructUnionSpecifier & s)override; 
+    };
+} // namespace c4::model::statement
